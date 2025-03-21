3187315b2edf31c76ba141329b4d6c75ab316704
Ahmad Nawab <ahmad.nawab@ecmwf.int>
2025-03-21 08:18:27 +0000

    NVHPC 24.5: fixup toolchain

--- package/bundle/arch/ecmwf/hpc2020/nvhpc/24.5/hpcx-openmpi/2.19.0-cuda/toolchain.cmake
+++ package/bundle/arch/ecmwf/hpc2020/nvhpc/24.5/hpcx-openmpi/2.19.0-cuda/toolchain.cmake
@@ -1,45 +1,42 @@
-####################################################################
-# COMPILER
-####################################################################
-
-set( CMAKE_C_COMPILER nvc )
-set( CMAKE_CXX_COMPILER nvc++ )
-set( CMAKE_Fortran_COMPILER nvfortran )
+# (C) Copyright 2022- ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
 
 ####################################################################
 # OpenMP FLAGS
 ####################################################################
 
-set( OpenMP_C_FLAGS             "-mp -mp=bind,allcores,numa" )
-set( OpenMP_CXX_FLAGS           "-mp -mp=bind,allcores,numa" )
-set( OpenMP_Fortran_FLAGS       "-mp -mp=gpu,bind,allcores,numa -gpu=cc80,lineinfo,fastmath,rdc" )
+if(ENABLE_OMP_OFFLOAD)
+  set( OpenMP_Fortran_FLAGS       "-mp -mp=gpu,bind,allcores,numa -gpu=cc80,lineinfo,fastmath,rdc" CACHE STRING "" FORCE)
+else()
+  set( OpenMP_Fortran_FLAGS       "-mp -mp=bind,allcores,numa" CACHE STRING "" FORCE)
+endif()
 
 ####################################################################
 # OpenAcc FLAGS
 ####################################################################
 
-# -mp=gpu
-set( OpenACC_Fortran_FLAGS "-acc=gpu -gpu=cc80,gvmode,lineinfo,fastmath,rdc" )
-# set( OpenACC_Fortran_FLAGS "-acc=gpu -gpu=cc80,gvmode,debug,rdc" )
-# set( OpenACC_Fortran_FLAGS "-acc=gpu -mp=gpu -gpu=cc80,gvmode,debug,rdc -Minfo=mp" )
-# set( OpenACC_Fortran_FLAGS "-acc=gpu -mp=gpu -gpu=cc80,gvmode,fastmath,rdc -Minfo=mp" )
-# set( OpenACC_Fortran_FLAGS "-acc=gpu -mp=gpu -gpu=gvmode,fastmath,rdc -Minfo=accel" )
+set( OpenACC_Fortran_FLAGS "-acc=gpu -gpu=cc80,lineinfo,fastmath,rdc" CACHE STRING "" FORCE)
+
+####################################################################
+# CUDA FLAGS
+####################################################################
+
+if(NOT DEFINED CMAKE_CUDA_ARCHITECTURES)
+  set(CMAKE_CUDA_ARCHITECTURES 80)
+endif()
 
 ####################################################################
 # COMMON FLAGS
 ####################################################################
 
-set( ECBUILD_Fortran_FLAGS "-fpic" )
-set( ECBUILD_Fortran_FLAGS "${ECBUILD_Fortran_FLAGS} -Mframe" )
+set( ECBUILD_Fortran_FLAGS "-Mframe" )
 set( ECBUILD_Fortran_FLAGS "${ECBUILD_Fortran_FLAGS} -Mbyteswapio" )
 set( ECBUILD_Fortran_FLAGS "${ECBUILD_Fortran_FLAGS} -Mstack_arrays" )
 set( ECBUILD_Fortran_FLAGS "${ECBUILD_Fortran_FLAGS} -Mrecursive" )
-set( ECBUILD_Fortran_FLAGS "${ECBUILD_Fortran_FLAGS} -Ktrap=fp" )
 set( ECBUILD_Fortran_FLAGS "${ECBUILD_Fortran_FLAGS} -Kieee" )
 set( ECBUILD_Fortran_FLAGS "${ECBUILD_Fortran_FLAGS} -Mdaz" )
-
-set( ECBUILD_Fortran_FLAGS_BIT "-O2 -gopt" )
-
-set( ECBUILD_C_FLAGS "-O2 -gopt -traceback" )
-
-set( ECBUILD_CXX_FLAGS "-O2 -gopt" )
