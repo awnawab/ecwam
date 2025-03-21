472383fd205074e5b337350ecc1087cf9e12262a
Ahmad Nawab <ahmad.nawab@ecmwf.int>
2025-03-21 21:54:25 +0200

    ROCM AFAR: update cmake setup for openmp offload

--- cmake/ecwam_compile_flags.cmake
+++ cmake/ecwam_compile_flags.cmake
@@ -47,7 +47,7 @@ elseif(CMAKE_Fortran_COMPILER_ID MATCHES "PGI|NVHPC")
 
 elseif(CMAKE_Fortran_COMPILER_ID MATCHES "Flang")
   set(autopromote_flags   "-fdefault-real-8")
-  set(fpe_flags           "-ffp-exception-behavior=strict")
+  set(fpe_flags           "-Xclang -ffp-exception-behavior=strict")
 
 endif()
 
@@ -102,7 +102,7 @@ if(CMAKE_Fortran_COMPILER_ID MATCHES "GNU")
   endif()
 endif()
 
-if(CMAKE_Fortran_COMPILER_ID MATCHES "Flang")
+if(CMAKE_Fortran_COMPILER_ID MATCHES "Flang" AND NOT CMAKE_Fortran_COMPILER MATCHES "amdflang")
   # Linker complains of unknown arguments:
   #    warning: argument unused during compilation: '-fdefault-real-8' [-Wunused-command-line-argument]
   foreach( LINKER_FLAGS CMAKE_EXE_LINKER_FLAGS CMAKE_SHARED_LINKER_FLAGS CMAKE_STATIC_LINKER_FLAGS )
